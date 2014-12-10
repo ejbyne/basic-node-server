36ad015940eb5fdb3f3cb2aee22488d1f614bab3
Alan Bridger <alanrbridger@gmail.com>
2014-12-10 17:23:16 +0000

    Adds port variable for Heroku

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ var expressLayouts = require('express-ejs-layouts');
 
 app.set('view engine', 'ejs');
 app.set('layout', 'layout');
-
-app.use(expressLayouts);
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.use(expressLayouts);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -25,8 +23,10 @@ app.post('/', function(request, response){
   response.render('welcome',{name:name, layout:'layout'});
 });
 
-server.listen(3000, function() {
-  console.log("Server listening on port 3000");
+var port = process.env.PORT || 3000;
+
+server.listen(port, function() {
+  console.log("Server listening on port " + port);
 });
 
 module.exports = server;
