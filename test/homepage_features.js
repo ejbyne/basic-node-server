3dae7025886de86a4649cbedf36fb8567205954f
Alan Bridger <alanrbridger@gmail.com>
2014-12-10 11:52:47 +0000

    First test fails as per walkthough

---
+++ test/homepage_features.js
@@ -0,0 +1,15 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('homepage', function(){
+  before(function(){
+    casper.start('http://localhost:3000/');
+  });
+
+  it('hello worlds', function(){
+    casper.then(function(){
+      expect("body").to.have.text("Hello world");
+    });
+  });
+
+});
